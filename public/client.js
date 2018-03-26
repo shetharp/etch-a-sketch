a0d3e9e164f12a4ed8c4be3d42618b503e3a9785
Arpit Sheth <shetharp@gmail.com>
2018-03-26 03:31:56 -0400

    debug linewidth

--- public/client.js
+++ public/client.js
@@ -19,6 +19,7 @@ var firstMessage=true;    // What the first message, to start on the first value
     
     socket.on('new-lnw', function(newWidth){
       radius = floor(newWidth / 10);
+      console.log("NEW LINE WIDTH!!!")
     });
     
     socket.on('new-pos', function(newPosition) { // handling new sensor values
