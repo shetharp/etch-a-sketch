b671a57ab6326d2595060ddf8f4d136056d9f63a
Arpit Sheth <shetharp@gmail.com>
2018-03-26 03:41:48 -0400

    Fixes error in logging line weight in console

--- public/client.js
+++ public/client.js
@@ -19,7 +19,7 @@ var firstMessage=true;    // What the first message, to start on the first value
     
     socket.on('new-lnw', function(newWidth){
       radius = floor(newWidth / 50);
-      console.log("New Line Weight: " + str(radius));
+      console.log("New Line Weight: " + radius);
     });
     
     socket.on('new-pos', function(newPosition) { // handling new sensor values
