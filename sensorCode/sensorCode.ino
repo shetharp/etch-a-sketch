4208519cef05c5d5c8f7661fc5ef4f7f526fc6f2
Arpit Sheth <shetharp@gmail.com>
2018-03-26 01:20:03 -0400

    X and Y potentiometers

--- sensorCode/sensorCode.ino
+++ sensorCode/sensorCode.ino
@@ -1,10 +1,13 @@
-#define SENSORPINA A0 // x axis
+#define SENSORPINX A4 // x axis
+#define SENSORPINY A3 // x axis
+
  //TODO: define other sensor inputs
 
 unsigned long targetTime=0;
-const unsigned long interval=2500; //TODO: How fast should we read
+const unsigned long interval=500; //TODO: How fast should we read
 void setup(){
 // TODO: begin the serial connection with a baudrate of 115200
+  Serial.begin(115200);
 }
 
 
@@ -17,8 +20,10 @@ void loop(){
      //TODO: convert values into a string https://www.arduino.cc/en/Tutorial/StringConstructors
 		 //TODO: combine them into a string that can be understood by server.js
 		 //TODO: send the string over serial
-
-
+     int PositionX = analogRead(SENSORPINX);
+     int PositionY = analogRead(SENSORPINY);
+     String data = String(PositionX) + "," + String(PositionY) + "\r\n";
+     Serial.print(data.c_str());
 	}
 	// TODO: Detect if you want to reset the screen(shake the etch-a-sketch)
   // TODO: write the reset message(see server.js) to the serial port
